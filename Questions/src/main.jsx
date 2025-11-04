a9211457a1d04ed9704c5c436c0548e96b4c3b15
nisha <nishantjaiswal0001@gmail.com>
2025-11-04 20:23:02 +0530

    bgchanger

--- Questions/src/main.jsx
+++ Questions/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import BGchange from './BGchanger.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+  <BGchange/>
   </StrictMode>,
 )
